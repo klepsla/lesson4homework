61b2ec22a8c450269b8349a18886f1caf2517a49
klepsla <kleslava3@gmail.com>
2022-07-14 17:24:49 +0300

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание № 1
+    int clientOS = 0;
+    if (clientOS == 0) {
+        System.out.println("Установите версию приложения для iOS по ссылке...");}
+    if (clientOS != 1) {
+        System.out.println("Установите версию приложениядля Android по ссылке...");}
+
+        //Задание № 2
+
     }
 
 }
